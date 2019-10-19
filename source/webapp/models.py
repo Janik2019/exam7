605749a8837d77af6a095237086d3d2b01d0a5ad
Janik2019 <lucky-lucky-7777@mail.ru>
2019-10-19 13:12:27 +0600

    Закончил первый этап контрольной работы

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=300, null=False, blank=False, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания опроса')
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
+
+
+class Choice(models.Model):
+    text = models.TextField(max_length=3000,null=False, blank=False, verbose_name='Текст варианта' )
+    poll = models.ForeignKey('webapp.Poll', related_name='choice', null=True, blank=False,
+                                on_delete=models.CASCADE, verbose_name='Опрос')
+
+
+    def __str__(self):
+        return self.text[:20]
+
+    class Meta:
+        verbose_name = 'Вариант ответа'
+        verbose_name_plural = 'Варианты ответа'
